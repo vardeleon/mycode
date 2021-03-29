7a21e67aa289552bdc572292f68cf4c6b330737d
vardeleon <rcvardeleon@gmail.com>
2021-03-29 18:40:49 +0000

    your commit message

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -11,4 +11,12 @@ print("The second item in the list (port): " + str(my_list[1]) )
 
 # display the third item in the list
 print("The last item in the list (state): " + my_list[2] )
+
+# create the new list of values
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# Display the new sentence
+# split into mult-line for readability
+print("When I " + new_list[5] + " into IP addresses " + new_list[3])
+print("or " + new_list[4] + " I am unable to ping ports")
+print(str(new_list[0]) + ", " + new_list[1] + ", or " + str(new_list[2]))

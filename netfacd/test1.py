4bff2d6cc473e5785c9aeb7928bec924d62f803a
vardeleon <rcvardeleon@gmail.com>
2021-03-31 21:22:07 +0000

    Learning to work with the matplotlib library

---
+++ netfacd/test1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import netifaces
+
+print(netifaces.interfaces())
+
+for i in netifaces.interfaces():
+    print('\n********Details of Interface - ' + i + ' ************')
+    try:
+        print((netifaces.ifaddresses(i)[netifaces.AF_LINK])[0]['addr']) # Prints the MAC address
+        print((netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
+    except:
+        print() # Print an error message
